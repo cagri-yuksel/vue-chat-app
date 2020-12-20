0b5c0580aa91c20847c0742b9df958a4ae8f260c
baristunar <tunarbaris7@gmail.com>
2020-12-20 22:44:22 +0300

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-      <login v-if="true" />
+  <login v-if="true" />
   <div class="container">
-    <div class="chat--app--container d-flex justify-content-between align-items-start">
+    <div
+      class="chat--app--container d-flex justify-content-between align-items-start"
+    >
       <!-- chatsection -->
       <!-- active users -->
     </div>
@@ -10,9 +12,21 @@
 
 <script>
 import Login from "./components/login.vue";
+import axios from "axios"
 export default {
   components: {
     Login,
+  },
+  created() {
+     axios
+      .get("http://localhost:3000/user")
+      .then((response) => {
+        console.log("response", response);
+        // this.comments = response.data.filter((c) => c.id <= 10);
+      })
+      .catch((e) => {
+        console.log("e", e);
+      });
   },
 };
 </script>
