afd9a6b72e93b6fbf5075ec4e24b76c6447f329d
redrodawn <cserdaryuksel@gmail.com>
2020-12-22 03:51:58 +0300

    improve

--- src/components/activeUsers.vue
+++ src/components/activeUsers.vue
@@ -1,26 +1,63 @@
 <template>
-<!-- örnek olabilir diye yaptım  -->
+  <!-- <p> active user: {{toUser}} </p> -->
+  <!-- örnek olabilir diye yaptım  -->
   <div class="card card-light w-40">
     <div class="card--header">
       <h3>Aktif Kullanıcılar</h3>
     </div>
-    <div class="card--body" v-for="i in userInfo" :key="i.id">
+    <div
+      class="card--body list-group"
+      @click="changeValue(i)"
+      v-for="i in userList"
+      :key="i.id"
+    >
       <div
         class="active-user-item d-flex justify-content-start align-items-center"
       >
-      <img class="rounded-circle mr-5" :src="i.img" />
-       
-        <div class="full_name" >
-          {{ i.username }}
-          
+        <div
+          class="badge d-flex justify-content-center align-items-center mr-5"
+        >
+          <img class="rounded-circle mr-5" :src="i.img" />
         </div>
+        <button
+          type="button"
+          class="list-group-item list-group-item-action"
+          aria-current="true"
+        >
+          {{ i.username }}
+        </button>
       </div>
     </div>
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
-  props: ["userInfo"],
+  data() {
+    return {
+      userList: [],
+      selectUser: [],
+      toUser: null,
+    };
+  },
+  methods: {
+    changeValue(e) {
+      this.toUser = e;
+      this.$emit("toUserChange", this.toUser);
+      console.log("changevalue  ", this.toUser.id);
+    },
+  },
+  created() {
+    axios
+      .get("http://localhost:3000/users")
+      .then((response) => {
+        console.log("active list users", response);
+        this.userList.push(...response.data);
+      })
+      .catch((e) => {
+        console.log("e", e);
+      });
+  },
 };
 </script>
 
--- src/components/chatSection.vue
+++ src/components/chatSection.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="card mr-10">
     <ul class="messages" ref="container">
-      <li
+      <li v-for="i in chatLog" :key="i">i.mesaj: {{ i.mesaj }}</li>
+      <li v-for="i in chatLog" :key="i">alt bölüm {{ i }}</li>
+    </ul>
+    <!--       <li
         v-for="user in users"
         :key="user.id"
         :class="{ 'current-user': activeUser }"
       >
         {{ user.message }}<small>14:55</small>
-      </li>
-    </ul>
+      </li> -->
+
     <div class="text-container d-flex justify-content-start align-items-start">
       <input @keypress.enter="testMethod" v-model="message" type="text" />
       <button :disabled="isDisable" class="btn-primary" @click="testMethod()">
@@ -19,10 +22,12 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  props: ["users", "activeUser"],
+  props: ["users", "userInfo", "changeToUser"],
   data() {
     return {
+      chatLog: [],
       message: "",
     };
   },
@@ -32,25 +37,35 @@ export default {
       let content = this.$refs.container;
       content.scrollTop = content.scrollHeight;
     },
-
-    testMethod() {
-    
-      this.$emit("send-message-event", this.message);
-      this.message = "";
-      console.log("mesaj", this.message);
-    },
   },
   computed: {
     isDisable() {
       return this.message == "" ? true : false;
     },
   },
+
   updated() {
     this.scrollToEnd();
   },
   mounted() {
     this.scrollToEnd();
   },
+  watch:{
+    changeToUser(n){
+      axios
+        .get(
+          "http://localhost:3000/chat?id="+this.userInfo[0].id+"&to="+this.changeToUser.id+"&to="+this.userInfo[0].id+"&id="+this.changeToUser.id
+        )
+        .then((response) => {
+          console.log("response chat", response);
+          this.chatLog = []
+          this.chatLog.push(...response.data);
+        })
+        .catch((e) => {
+          console.log("e", e);
+        });
+    }
+  }
 };
 </script>
 
