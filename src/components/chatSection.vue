44c6b2e90005337fa6e580a9eaaf76a83f324e9b
baristunar <tunarbaris7@gmail.com>
2020-12-26 02:27:47 +0300

    "saat eklendi"

--- src/components/chatSection.vue
+++ src/components/chatSection.vue
@@ -8,6 +8,7 @@
         class="text-color-black"
       >
         {{ i.mesaj }}
+        <small>{{i.time}}</small>
       </li>
     </ul>
     <div class="text-container d-flex justify-content-start align-items-start">
@@ -36,27 +37,33 @@ export default {
       content.scrollTop = content.scrollHeight;
     },
     sendMessage() {
-      while(this.message !== ""){
-         let chat = {
-        userID: this.userInfo[0].userID,
-        mesaj: this.message,
-        to: this.changeToUser.userID,
-        date: "UTC",
-      };
-      this.message = ""
+      const currentTime = new Date();
+      const currentHour = currentTime.getHours();
+      const currentMinute = currentTime.getMinutes();
 
-      axios
-        .post("http://localhost:3000/chat", chat)
-        .then((save_message) => {
-          console.log("save_message", save_message);
-          this.chatLog.push(save_message.data);
-        })
-        .catch((e) => {
-          console.log("e ", e)
-        });
-      console.log(chat);
+      while (this.message !== "") {
+        let chat = {
+          userID: this.userInfo[0].userID,
+          mesaj: this.message,
+          to: this.changeToUser.userID,
+          time:
+            currentMinute < 0
+              ? currentHour + ":0" + currentMinute
+              : currentHour + ":" + currentMinute,
+        };
+        this.message = "";
+
+        axios
+          .post("http://localhost:3000/chat", chat)
+          .then((save_message) => {
+            console.log("save_message", save_message);
+            this.chatLog.push(save_message.data);
+          })
+          .catch((e) => {
+            console.log("e ", e);
+          });
+        console.log(chat);
       }
-     
     },
   },
   computed: {
@@ -64,7 +71,7 @@ export default {
       return this.message == "" ? true : false;
     },
   },
- /*  created(){
+  /*  created(){
 axios
         .get(
           "http://localhost:3000/chat?id=" +
@@ -95,23 +102,29 @@ axios
     changeToUser() {
       axios
         .get(
-          "http://localhost:3000/chat?userID="+this.userInfo[0].userID+"&to="+this.changeToUser.userID+"&to="+this.userInfo[0].userID+"&userID="+this.changeToUser.userID
+          "http://localhost:3000/chat?userID=" +
+            this.userInfo[0].userID +
+            "&to=" +
+            this.changeToUser.userID +
+            "&to=" +
+            this.userInfo[0].userID +
+            "&userID=" +
+            this.changeToUser.userID
         )
         .then((response) => {
           console.log("response chat", response);
-          this.chatLog = []
+          this.chatLog = [];
           this.chatLog.push(...response.data);
         })
         .catch((e) => {
           console.log("e", e);
-        })
+        });
     },
   },
 };
 </script>
 <style scoped>
-
-.text-color-black{
+.text-color-black {
   color: black;
 }
 </style>
